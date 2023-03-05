d0fabdccff9d5b58263f85f15de0f941a48ac237
schahin0001 <schahinheidari@gmail.com>
2023-03-05 20:39:27 +0100

    added histogram and flights and table file of bokeh

--- bokeh_server/main.py
+++ bokeh_server/main.py
@@ -9,13 +9,16 @@ res = p.text(x=[], y=[], text=[], text_color=[])
 dataSource = res.data_source
 
 button = Button(label= 'registration')
-
+i = 0
 def clickOnSubmit():
+    global i
     newData = dict()
     newData['x'] = dataSource.data['x'] + [random() * 70 + 15] # we could use any number instead of 15
     newData['y'] = dataSource.data['y'] + [random() * 70 + 15]
+    newData['text_color'] = dataSource.data['text_color'] + [RdYlBu3[i%3]]
     newData['text'] = dataSource.data['text'] + [str(random())]
     dataSource.data = newData
+    i += 1
 
 button.on_click(clickOnSubmit)
 
